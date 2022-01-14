a8a9d7d31c8812fe2e4c405365f57bcabaa23ab1
Geelpa <eduardo.augusto157@hotmail.com>
2022-01-13 23:00:29 -0300

    a looooot of progress

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
-document.querySelector("square-btn").onclick = function() {
-    document.querySelector('modal').style.display = 'none';
+
+
+//  PROGRAMAÇÃO ORIENTADA A OBJETOS //
+const Modal = {
+    toggle(){
+        document
+        .querySelector('.modal-overlay')
+        .classList.toggle('hide')
+    }
+}
+
+
+const Form = {
+    name: document.querySelector('input#name'),
+    link: document.querySelector('input#link'),
+
+    getValues() {
+        return {
+            name: Form.name.value,
+            link: Form.name.link,
+        }
+    },
+
+    validateFields() {
+        const { name, link } = Form.values()
+
+        if (name.trim() === "" ||
+            link.trim() === "") {
+            throw new Error("Preencha todos os campos.")
+        }
+    },
+
+    clearFields() {
+        Form.name.value = ""
+        Form.link.value = ""
+    },
+
+    submit(event) {
+        event.preventDefault()
+        try {
+            Form.validateFields()
+            Form.clearFields()
+            Modal.close()
+        } catch (error) {
+            alert(error.message)
+        }
+    }
 }
