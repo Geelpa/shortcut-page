84a628dd98d8d2e5f3424880d7658cdf9ce26ec0
Geelpa <eduardo.augusto157@hotmail.com>
2022-01-16 04:25:32 -0300

    idk tbh

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-
-
 //  PROGRAMAÇÃO ORIENTADA A OBJETOS //
 const Modal = {
-    toggle(){
+    toggle() {
         document
-        .querySelector('.modal-overlay')
-        .classList.toggle('hide')
+            .querySelector('.modal-overlay')
+            .classList.toggle('hide')
     }
 }
 
+const DOM = {
+    shortcutContainer: document.querySelector('.container'),
+
+    addShortcut(shortcut, index) {
+        const sc = document.createElement('a')
+        sc.innerHTML = DOM.innerHTMLShortcut(shortcut, index)
+        sc.dataset.index = index
+
+        DOM.shortcutContainer.appendChild(sc)
+    },
+
+    innerHTMLShortcut(shortcut, index) {
+        const html = `
+            <a href="#" class="add_btn" onclick="Modal.toggle()">
+                <i class="fas fa-plus"></i>
+            </a>
+         `
+        return html
+    }
+
+    // addTransaction (transaction, index) {
+    //     const tr = document.createElement('tr')
+    //     tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+    //     tr.dataset.index = index
+    //     DOM.transactionsContainer.appendChild(tr)
+    // },
+}
 
 const Form = {
     name: document.querySelector('input#name'),
@@ -17,15 +42,15 @@ const Form = {
     getValues() {
         return {
             name: Form.name.value,
-            link: Form.name.link,
+            link: Form.link.value,
         }
     },
 
     validateFields() {
-        const { name, link } = Form.values()
+        const { name, link } = Form.getValues()
 
-        if (name.trim() === "" ||
-            link.trim() === "") {
+        if (name.trim() === " " ||
+            link.trim() === " ") {
             throw new Error("Preencha todos os campos.")
         }
     },
@@ -37,10 +62,12 @@ const Form = {
 
     submit(event) {
         event.preventDefault()
+        
         try {
             Form.validateFields()
             Form.clearFields()
-            Modal.close()
+            Form.addShortcut()
+            Modal.toggle()
         } catch (error) {
             alert(error.message)
         }
